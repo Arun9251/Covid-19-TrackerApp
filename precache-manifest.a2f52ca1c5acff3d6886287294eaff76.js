86d6290a4b717c645e5b304f2c351ab734b0500e
Arun Singh <77932309+Arun9251@users.noreply.github.com>
2021-04-13 02:25:06 +0530

    Updates

---
+++ precache-manifest.a2f52ca1c5acff3d6886287294eaff76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5416e516e6d41c92960e258c6e255d8",
+    "url": "/Covid-19-TrackerApp/index.html"
+  },
+  {
+    "revision": "5cb101cc30f8cbc96edd",
+    "url": "/Covid-19-TrackerApp/static/css/main.782ea8fc.chunk.css"
+  },
+  {
+    "revision": "18ed6940f9d12f82fabf",
+    "url": "/Covid-19-TrackerApp/static/js/2.e51bda2b.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-19-TrackerApp/static/js/2.e51bda2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb101cc30f8cbc96edd",
+    "url": "/Covid-19-TrackerApp/static/js/main.f26fdd70.chunk.js"
+  },
+  {
+    "revision": "9e81775f9733e9ac299b",
+    "url": "/Covid-19-TrackerApp/static/js/runtime-main.356f4cba.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-TrackerApp/static/media/covid19.d7265326.png"
+  }
+]);
